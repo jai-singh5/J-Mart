46ef363e917a9f87d4f7ede8ffe8a6e5d5eb31ef
jai-singh5 <jaisingh09520@gmail.com>
2024-10-18 20:24:43 +0530

    Completed project

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -27,32 +27,32 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link" aria-current="page" to="/">
+                <NavLink className="nav-link fw-medium" aria-current="page" to="/">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/products">
+                <NavLink className="nav-link fw-medium" to="/products">
                   Products
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
+                <NavLink className="nav-link fw-medium" to="/about">
                   About
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
+                <NavLink className="nav-link fw-medium" to="/contact">
                   Contact
                 </NavLink>
               </li>
             </ul>
            <div className="buttons">
-            <NavLink to="/login" className="btn btn-outline-light">
+            <NavLink to="/login" className="btn btn-outline-dark">
              <i className="fa fa-sign-in me-1"></i> Login</NavLink>
-             <NavLink to="/register" className="btn btn-outline-light ms-2">
+             <NavLink to="/register" className="btn btn-outline-dark ms-2">
              <i className="fa fa-user-plus me-1"></i> Register</NavLink>
-             <NavLink to="/cart" className="btn btn-outline-light ms-2">
+             <NavLink to="/cart" className="btn btn-outline-dark ms-2">
              <i className="fa fa-shopping-cart me-1"></i> Cart ({state.length})</NavLink>
            </div>
           </div>
--- src/component/Products.js
+++ src/component/Products.js
@@ -85,7 +85,7 @@ const Products = () => {
   };
   return (
     <div>
-      <div className="container my-5 py-5">
+      <div className="container  py-5">
         <div className="row">
           <div className="col-12 mb-5">
             <h1 className="display-6 fw-border text-center">Latest Products</h1>
